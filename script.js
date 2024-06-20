e12c7f406928a21477dae6e355e4b4c467c9897b
Walid Saifi <saifiwalid21@gmail.com>
2024-06-20 11:01:54 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,145 @@
+
+const ajouter = document.getElementById("enregistrement-client");
+if(ajouter){
+ajouter.addEventListener('submit',function(event){
+     event.preventDefault();
+     var tva = document.getElementById("tva").value;
+     var nom = document.getElementById("nom").value;
+     var tel = document.getElementById("telephone").value;
+     var adresse = document.getElementById("adresse").value;
+
+     var messageTva = document.getElementById("messageTva");
+     var messageNom = document.getElementById("messageNom");
+     var messageTel = document.getElementById("messageTel");
+     var messageAdresse = document.getElementById("messageAdre");
+     var messageAjouter = document.getElementById("messageAjouter");
+     
+     messageTva.innerText = '';
+     messageNom.innerText = '';
+     messageTel.innerText = '';
+     messageAdresse.innerText = '';
+     messageAjouter.innerText = '';
+    
+    console.log("testing");
+   
+     var validation=true;
+
+     if(tva.length !== 9) {
+             messageTva.innerText='Attention, TVA est fixée à 9 caractères.';
+             messageTva.style.color='red';
+             validation=false;
+     }
+     if (nom === '') {
+        messageNom.innerText = 'Le nom ne peut pas être vide.';
+        messageNom.style.color = 'red';
+        validation = false;
+    }
+    if (tel === '') {
+        messageTel.innerText = 'Le numéro de téléphone ne peut pas être vide.';
+        messageTel.style.color = 'red';
+        validation = false;
+    }
+    
+    if (adresse === '') {
+        messageAdresse.innerText = 'L\'adresse ne peut pas être vide.';
+        messageAdresse.style.color = 'red';
+        validation = false;
+    }
+
+
+     if(validation){
+       
+        var noms = JSON.parse(window.localStorage.getItem('noms'));
+        noms.push(nom);
+
+        window.localStorage.setItem('noms', JSON.stringify(noms));
+        messageAjouter.innerText = 'Client ajouté avec succès.';
+        messageAjouter.style.color = 'green';
+
+ }
+
+
+
+ const vider=document.getElementById('vider');
+
+ vider.addEventListener('click', function(){
+    tva.innerText='';
+    nom.innerText= '';
+    tel.innerText= '';
+    adresse.innerText= '';
+ });
+
+
+
+});
+}else{
+    console.log("ddddddddddddddddddddddddd")
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var expediteur = document.getElementById("expediteur");
+    var destinataire = document.getElementById("destinataire");
+
+    var noms = JSON.parse(window.localStorage.getItem('noms'));
+
+    console.log('Clients chargés depuis localStorage:',noms); 
+
+    for (var i = 0; i < noms.length; i++) {
+        var client = noms[i];
+        var optionExpediteur = document.createElement('option');
+        optionExpediteur.value = client;
+        optionExpediteur.textContent = client;
+        expediteur.appendChild(optionExpediteur);
+    
+        var optionDestinataire = document.createElement('option');
+        optionDestinataire.value = client;
+        optionDestinataire.textContent = client;
+        destinataire.appendChild(optionDestinataire);
+    }
+    
+});
+document.addEventListener('DOMContentLoaded', function() {
+    const orderForm = document.getElementById("orderForm");
+    const messageClient = document.getElementById("messageClient");
+    const messagePoids = document.getElementById("messagePoids");
+    const messageCommandes = document.getElementById("messageCommandes");
+
+    orderForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        var destinataire = document.getElementById("destinataire").value;
+        var expediteur = document.getElementById("expediteur").value;
+        var poids = document.getElementById("poids").value;
+       
+
+        messageClient.innerText = '';
+        messagePoids.innerText = '';
+        messageCommandes.innerText = '';
+       
+
+        var validation = true;
+
+        if (destinataire === expediteur) {
+            messageClient.innerText = 'Sélectionnez différents clients pour ajouter des commandes.';
+            messageClient.style.color = 'red';
+            validation = false;
+        }
+
+        var poidsNombre = parseFloat(poids);
+
+        if (isNaN(poidsNombre) || poidsNombre < 0) {
+            messagePoids.innerText = 'Le poids doit être un nombre positif !';
+            messagePoids.style.color = 'red';
+            validation = false;
+        }
+
+        if (validation) {
+            messageCommandes.innerText = 'Commande ajoutée avec succès.';
+            messageCommandes.style.color = 'green';
+        }
+    });
+});
+
+
+
